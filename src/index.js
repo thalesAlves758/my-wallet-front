c6593ea21939656af0db34ac6fbb195e97990587
Thales Alves <thalesalves758@gmail.com>
2022-07-01 13:50:35 -0300

    Import reset css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import './assets/css/reset.css';
+
 import App from './components/App';
 
 ReactDOM.render(<App />, document.querySelector('.root'));
