2597dd9a2a9bcfd268bc096318feab465b43fe61
Thales Alves <thalesalves758@gmail.com>
2022-07-01 16:54:59 -0300

    Create routes and import SignUp page and use it

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import SignUp from './SignUp';
 
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
