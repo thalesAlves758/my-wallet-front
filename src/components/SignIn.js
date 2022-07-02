ea91dda7d44112b3a524a900fa639beaa1d9a668
Thales Alves <thalesalves758@gmail.com>
2022-07-01 23:48:50 -0300

    Create SignIn page

---
+++ src/components/SignIn.js
@@ -0,0 +1,88 @@
+import React, { useState, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+import Button from './layout/Button';
+import Container from './layout/Container';
+import Form from './layout/Form';
+import InputForm from './layout/InputForm';
+import Main from './layout/Main';
+import StyledLink from './layout/StyledLink';
+import AppName from './shared/AppName';
+import UserContext from '../contexts/UserContext';
+import httpStatus from '../utils/httpStatus';
+
+function SignInForm() {
+  const API_URL = process.env.REACT_APP_API_URL;
+
+  const { setUser } = useContext(UserContext);
+
+  const navigate = useNavigate();
+
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+  });
+
+  function signIn() {
+    axios
+      .post(`${API_URL}/sign-in`, form)
+      .then(({ data }) => {
+        setUser({ email: form.email, token: data.token });
+        navigate('/');
+      })
+      .catch(({ response }) => {
+        if (response.status === httpStatus.UNAUTHORIZED) {
+          alert('Email ou senha incorretos. Tente novamente!');
+        }
+      });
+  }
+
+  function handleForm(event) {
+    setForm({ ...form, [event.target.name]: event.target.value });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    signIn();
+  }
+
+  return (
+    <Form onSubmit={(event) => handleSubmit(event)}>
+      <InputForm
+        placeholder="E-mail"
+        name="email"
+        type="email"
+        required
+        value={form.email}
+        onChange={(event) => handleForm(event)}
+      />
+
+      <InputForm
+        placeholder="Senha"
+        name="password"
+        type="password"
+        required
+        value={form.password}
+        onChange={(event) => handleForm(event)}
+      />
+
+      <Button type="submit">Entrar</Button>
+    </Form>
+  );
+}
+
+function SignIn() {
+  return (
+    <Container>
+      <Main>
+        <AppName />
+        <SignInForm />
+        <StyledLink to="/sign-up">Primeira vez? Cadastre-se!</StyledLink>
+      </Main>
+    </Container>
+  );
+}
+
+export default SignIn;
