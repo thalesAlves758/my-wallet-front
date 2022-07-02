811a79c68d3d9ce0ea4d48d6f849b0adaaeb1f65
Thales Alves <thalesalves758@gmail.com>
2022-07-02 13:24:16 -0300

    Add align and height properties

--- src/components/layout/Main.js
+++ src/components/layout/Main.js
@@ -3,8 +3,10 @@ import styled from 'styled-components';
 const Main = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: center;
   align-items: center;
   width: 100%;
+  height: 100%;
 `;
 
 export default Main;
