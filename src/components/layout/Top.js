345eb4aaafd591bbf214b4eecc47c6d3adf83adf
Thales Alves <thalesalves758@gmail.com>
2022-07-02 13:24:37 -0300

    Create Top component

---
+++ src/components/layout/Top.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const Top = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #ffffff;
+  font-size: 26px;
+  font-weight: 700;
+
+  ion-icon {
+    font-size: 36px;
+
+    :hover {
+      cursor: pointer;
+    }
+  }
+`;
+
+export default Top;
