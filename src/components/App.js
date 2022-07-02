bb319bc02eea8c21b5ebc0df19442f599d2c88a0
Thales Alves <thalesalves758@gmail.com>
2022-07-01 23:49:13 -0300

    Use User context and SignIn page

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import UserContext from '../contexts/UserContext';
+
 import SignUp from './SignUp';
+import SignIn from './SignIn';
 
 function App() {
+  const [user, setUser] = useState(null);
+
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/sign-up" element={<SignUp />} />
-      </Routes>
+      <UserContext.Provider value={{ user, setUser }}>
+        <Routes>
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/sign-in" element={<SignIn />} />
+        </Routes>
+      </UserContext.Provider>
     </BrowserRouter>
   );
 }
