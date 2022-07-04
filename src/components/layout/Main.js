fb5ed656994f813994a769064db9dfbb601cdf75
Thales Alves <thalesalves758@gmail.com>
2022-07-04 00:46:50 -0300

    Add props

--- src/components/layout/Main.js
+++ src/components/layout/Main.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Main = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: ${(props) => (props.notCenter ? 'flex-start' : 'center')};
   align-items: center;
   width: 100%;
   height: 100%;
