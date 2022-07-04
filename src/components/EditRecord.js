7057d15358a84ef839ee7b56debda69834cb7265
Thales Alves <thalesalves758@gmail.com>
2022-07-04 12:47:29 -0300

    Create edit record page

---
+++ src/components/EditRecord.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useContext } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+
+import UserContext from '../contexts/UserContext';
+import Container from './layout/Container';
+import Main from './layout/Main';
+import Top from './layout/Top';
+import RecordForm from './shared/RecordForm';
+
+function EditRecord({ type = 'input' }) {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const { user } = useContext(UserContext);
+
+  useEffect(() => {
+    if (user.token === '') {
+      navigate('/sign-in');
+    }
+  }, []);
+
+  return (
+    <Container>
+      <Main notCenter>
+        <Top>Editar {type === 'input' ? 'entrada' : 'saída'}</Top>
+        <RecordForm
+          id={location.state.id}
+          value={location.state.value}
+          description={location.state.description}
+          type={type}
+        />
+      </Main>
+    </Container>
+  );
+}
+
+export default EditRecord;
