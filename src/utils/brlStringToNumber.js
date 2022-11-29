c03d910c51a1249563126deb9c0ede369d8e932f
Thales Alves <thalesalves758@gmail.com>
2022-11-29 18:46:15 -0300

    feat: check argument type

--- src/utils/brlStringToNumber.js
+++ src/utils/brlStringToNumber.js
@@ -1,5 +1,5 @@
 function brlStringToNumber(string) {
-  return Number(string?.replace('.', '')?.replace(',', '.'));
+  return typeof string === 'string' ? Number(string?.replace('.', '')?.replace(',', '.')) : string;
 }
 
 export default brlStringToNumber;
