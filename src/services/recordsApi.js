39e188982da323533bb78dcd158a391458d5b16b
Thales Alves <thalesalves758@gmail.com>
2022-11-29 15:30:54 -0300

    feat: create delete record by id function

--- src/services/recordsApi.js
+++ src/services/recordsApi.js
@@ -9,3 +9,13 @@ export async function getRecords({ token }) {
 
   return { data, status };
 }
+
+export async function deleteRecordById({ token, recordId }) {
+  const { data, status } = await api.delete(`/records/${recordId}`, {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  });
+
+  return { data, status };
+}
