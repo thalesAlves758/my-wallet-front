615b6a98b444976a3c30535bb9ae1f60f6a82016
Thales Alves <thalesalves758@gmail.com>
2022-11-29 15:35:11 -0300

    refactor: remove use effect

--- src/components/NewInput.js
+++ src/components/NewInput.js
@@ -1,23 +1,11 @@
-import React, { useEffect, useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
 
-import UserContext from '../contexts/UserContext';
 import Container from './layout/Container';
 import Main from './layout/Main';
 import Top from './layout/Top';
 import RecordForm from './shared/RecordForm';
 
 function NewInput() {
-  const navigate = useNavigate();
-
-  const { user } = useContext(UserContext);
-
-  useEffect(() => {
-    if (user.token === '') {
-      navigate('/sign-in');
-    }
-  }, []);
-
   return (
     <Container>
       <Main notCenter>
